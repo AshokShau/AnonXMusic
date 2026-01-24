e28d3c9feea07817316d5c9cb0b62c2bdf9a2cd0
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-01-24 18:34:57 +0530

    ADD API

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -13,8 +13,10 @@ from pathlib import Path
 
 from py_yt import Playlist, VideosSearch
 
-from anony import logger
+from anony import logger, config
 from anony.helpers import Track, utils
+
+from ._api import FallenApi
 
 
 class YouTube:
@@ -24,6 +26,7 @@ class YouTube:
         self.checked = False
         self.cookie_dir = "anony/cookies"
         self.warned = False
+        self.fallen = FallenApi()
         self.regex = re.compile(
             r"(https?://)?(www\.|m\.|music\.)?"
             r"(youtube\.com/(watch\?v=|shorts/|playlist\?list=)|youtu\.be/)"
@@ -101,6 +104,10 @@ class YouTube:
 
     async def download(self, video_id: str, video: bool = False) -> str | None:
         url = self.base + video_id
+        if not video and config.API_KEY and config.API_URL:
+            if file_path := await self.fallen.download_track(url):
+                return file_path
+
         ext = "mp4" if video else "webm"
         filename = f"downloads/{video_id}.{ext}"
 
